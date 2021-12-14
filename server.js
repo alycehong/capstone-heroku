eb66bff8bc2380635dcb9da2765c045d9a35da1f
alycehong <jah6zd@virginia.edu>
2021-12-14 16:36:08 -0500

    changed port

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const path = require('path');
 const express = require('express');
 var app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 app.get("/test", (req, res) => {
     res.json({ message: "Hello from server!" });
+    // console.log("hello from server");
   });
-
+  
   app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
