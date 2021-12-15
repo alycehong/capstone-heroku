27452ecdf76f4c9b8c002d5b35032d430e498f0e
alycehong <jah6zd@virginia.edu>
2021-12-14 22:04:24 -0500

    took out proxy

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const path = require('path');
+// const path = require('path');
 const express = require('express');
-var app = express();
+const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
 
 app.use(express.static(__dirname + '/public'));
-app.get("/test", (req, res) => {
-    res.json({ message: "Hello from server!" });
+app.get("/backend", (req, res) => {
+    res.send({ message: "Hello from server!" });
     // console.log("hello from server");
   });
   
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+// // Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// // All other GET requests not handled before will return our React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'test.html'));
+// });
